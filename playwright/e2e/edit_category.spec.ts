60c8e59cdeab1cbd585942e33f33b17ad1b02625
martin-luik <martinluik@hotmail.com>
2023-09-07 13:16:07 +0300

    Improved e2e tests and translations

--- playwright/e2e/edit_category.spec.ts
+++ playwright/e2e/edit_category.spec.ts
@@ -7,23 +7,30 @@ test('Edit category', async ({ page }) => {
   await page.getByLabel('Name: *').fill('New category 1');
   await page.getByLabel('Agree to terms *').check();
   await page.getByRole('button', { name: 'Save' }).click();
-  await page.getByRole('navigation').getByRole('button', { name: 'Add category' }).click();
+
+  await expect(page.getByText('New category 1')).toBeVisible();
+
+  await page.getByRole('button', { name: 'Add root category' }).click();
   await page.getByLabel('Name: *').click();
   await page.getByLabel('Name: *').fill('New category 2');
   await page.getByLabel('Agree to terms *').check();
   await page.getByRole('button', { name: 'Save' }).click();
-  await page.getByRole('button', { name: 'New category 2' }).click();
-  await page.getByRole('button', { name: 'Edit category' }).click();
 
-  await page.getByLabel('Name:').click();
-  await page.getByLabel('Name:').fill('');
-  await page.getByRole('button', { name: 'Save' }).click();
-  await expect(page.getByText('* Name is required')).toBeVisible();
+  await expect(page.getByText('New category 2')).toBeVisible();
 
-  await page.getByLabel('Name:').fill('New subcategory 1');
-  await page.getByLabel('Category:').selectOption({ label: 'New category 1' });
-  await page.getByRole('button', { name: 'Save' }).click();
-  await page.getByRole('button', { name: 'New category 1' }).click();
-  await page.getByRole('button', { name: 'Delete category' }).click();
+
+   await page.getByRole('button', { name: 'New category 2' }).click();
+   await page.getByRole('button', { name: 'Edit category' }).click();
+
+   await page.getByLabel('Name:').click();
+   await page.getByLabel('Name:').fill('');
+   await page.getByRole('button', { name: 'Save' }).click();
+   await expect(page.getByText('* Name is required')).toBeVisible();
+
+   await page.getByLabel('Name:').fill('New subcategory 1');
+   await page.getByLabel('Category:').selectOption({ label: 'New category 1' });
+   await page.getByRole('button', { name: 'Save' }).click();
+   await page.getByRole('button', { name: 'New category 1' }).click();
+   await page.getByRole('button', { name: 'Delete category' }).click();
 });
 
